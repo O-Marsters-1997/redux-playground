a60cef4f5d4aa3be854db67778d0154c33fc7432
O-Marsters-1997 <olly.marsters@yahoo.co.uk>
2022-05-10 14:15:48 +0100

    designed basic header

--- redux-playground/src/components/Header.jsx
+++ redux-playground/src/components/Header.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { styled } from "@mui/system";
 import Typography from "@mui/material/Typography";
 import AppBar from "@mui/material/AppBar";
 import Toolbar from "@mui/material/Toolbar";
@@ -7,22 +8,29 @@ import Button from "@mui/material/Button";
 import IconButton from "@mui/material/IconButton";
 import MenuIcon from "@mui/icons-material/Menu";
 
+const MyComponent = styled("AppBar")(({ theme }) => ({
+  display: "flex",
+  flexDirection: "row",
+  justifyContent: "flex-end",
+  width: "100%",
+}));
+
 const Header = () => {
-
-
   return (
     <Box component="div" sx={{ flexGrow: 1 }}>
       <AppBar position="static">
         <Toolbar>
-          <IconButton
-            edge="start"
-            color="inherit"
-            aria-label="menu"
-            sx={{ mr: 2 }}
-          >
-            <MenuIcon />
-          </IconButton>
-          <Button variant = "header"> Sign out</Button>
+          <MyComponent>
+            <IconButton
+              edge="start"
+              color="inherit"
+              aria-label="menu"
+              sx={{ mr: "auto" }}
+            >
+              <MenuIcon />
+            </IconButton>
+            <Button variant="header"> Sign out</Button>
+          </MyComponent>
         </Toolbar>
       </AppBar>
     </Box>
